844227289e68cf671971325d68daa5981eb42c16
Alikadze <kilpero84@gmail.com>
2024-09-21 20:47:27 +0400

    turned components to signals

--- src/app/pages/add-game/add-game.component.ts
+++ src/app/pages/add-game/add-game.component.ts
@@ -8,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { GameFacade } from '../../core/facades/game.facade';
 import { GamePayload } from '../../core/interfaces/game';
-import { catchError, tap, throwError } from 'rxjs';
+import { catchError, Subscription, tap, throwError } from 'rxjs';
 import { GenreFacade } from '../../core/facades/genre.facade';
 import { AsyncPipe } from '@angular/common';
 import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogModule, MatDialogTitle } from '@angular/material/dialog';
@@ -40,25 +40,25 @@ import { Router } from '@angular/router';
 })
 export class AddGameComponent {
   gameFacade = inject(GameFacade);
-  gerneFacade = inject(GenreFacade);
+  genreFacade = inject(GenreFacade);
   router = inject(Router);
 
-  genres$ = this.gerneFacade.getGenres();
+  genres$ = this.genreFacade.getGenres();
 
   readonly dialog = inject(MatDialog);
+  private subscriptions = new Subscription();
 
   openDialog() {
     this.dialog.open(ConfirmDialogComponent);
   }
-
 
   newDate = new Date();
 
   form = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.maxLength(50)]),
     genreId: new FormControl(0, [Validators.required]),
-    price: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(100)]),
-    releaseDate: new FormControl(new Date() ,Validators.required),
+    price: new FormControl<null | number>(null, [Validators.required, Validators.min(0), Validators.max(100)]),
+    releaseDate: new FormControl<Date | null>(null, Validators.required),
   });
 
   createGame() {
@@ -66,14 +66,13 @@ export class AddGameComponent {
       console.log('Form is invalid');
       return;
     }
-  
+
     const { name, genreId, price, releaseDate } = this.form.value as { name: string, genreId: number, price: number, releaseDate: Date };
-  
-    // Format releaseDate to 'YYYY-MM-DD'
-    const formattedDate = releaseDate ? 
-      `${releaseDate.getFullYear()}-${(releaseDate.getMonth() + 1).toString().padStart(2, '0')}-${releaseDate.getDate().toString().padStart(2, '0')}` 
+
+    const formattedDate = releaseDate ?
+      `${releaseDate.getFullYear()}-${(releaseDate.getMonth() + 1).toString().padStart(2, '0')}-${releaseDate.getDate().toString().padStart(2, '0')}`
       : null;
-  
+
     const gamePayload: GamePayload = {
       name,
       genreId,
@@ -81,19 +80,23 @@ export class AddGameComponent {
       releaseDate: formattedDate
     };
 
-  
-    this.gameFacade.createGame(gamePayload).pipe(
+    const subscription = this.gameFacade.createGame(gamePayload).pipe(
       catchError((err) => {
         return throwError(() => err);
       }),
       tap(() => {
-        console.log('Game created');    
+        console.log('Game created');
         this.form.reset();
-        this.form.untouched;
         this.router.navigate(['/']);
         this.openDialog();
       })
     ).subscribe();
+
+    this.subscriptions.add(subscription);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
 }
